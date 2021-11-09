f911d70668647bf332821fa085c186e8f309ce3e
Haru825 <43505730+Haru825@users.noreply.github.com>
2021-11-08 19:56:46 -0800

    JSON CSV add

--- myTranslate.py
+++ myTranslate.py
@@ -2,22 +2,54 @@ from google_trans_new import google_translator
 import sys, getopt
 import webbrowser
 import os
+import json
+import csv
 
 def batext(tatext,language):
-    #print(googletrans.LANGUAGES)
-
     translator = google_translator()
     takentext= str(tatext)
     lang=str(language)
-    #lang = 'es'
-    #out = "hi"
     out=translator.translate(takentext, lang)
     #print("Original Text:", out.origin)
     #print("Translated Text:", out.text)
+    
     return out
+
+def jsontext(tatext,language):
+    translator = google_translator()
+    takentext= str(tatext)
+    lang=str(language)
+    out=translator.translate(takentext, lang)
+    
+    langdic={
+        "Original text": takentext,
+        "Language": lang,
+        "New text": out
+    }
+    return json.dumps(langdic)
+
+def csvtext(tatext):
+    translator = google_translator()
+    takentext= str(tatext)
+    lang="nl"
+    out=translator.translate(takentext, lang)
+    csv_columns=['Original Text','Language','Translation']
+    langdic={
+        "Original text": takentext,
+        "Language": lang,
+        "New text": out
+    }
+    
+    with open('translate.csv', 'w') as f:
+        for key in langdic.keys():
+            f.write("%s, %s\n" % (key, langdic[key]))
 
 if __name__ == "__main__":
     pie=batext('hi','es')
+    print(pie)
+    pie=jsontext('hi','es')
+    print(pie)
+    pie=csvtext('hi')
     print(pie)
 
 
