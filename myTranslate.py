c1496f02c321bea1b2e68c2abb5c53c4690d9edf
Haru825 <43505730+Haru825@users.noreply.github.com>
2021-11-09 14:36:52 -0800

    Add files via upload

--- myTranslate.py
+++ myTranslate.py
@@ -28,29 +28,30 @@ def jsontext(tatext,language):
     }
     return json.dumps(langdic)
 
-def csvtext(tatext):
+def csvtext(tatext, language):
     translator = google_translator()
     takentext= str(tatext)
-    lang="nl"
-    out=translator.translate(takentext, lang)
+    lang=str(language)
+    out=translator.translate(tatext, language)
     csv_columns=['Original Text','Language','Translation']
     langdic={
-        "Original text": takentext,
-        "Language": lang,
-        "New text": out
+        "Original text":tatext,
+        "Language":lang,
+        "Translation":out
     }
     
     with open('translate.csv', 'w') as f:
         for key in langdic.keys():
             f.write("%s, %s\n" % (key, langdic[key]))
+    
 
 if __name__ == "__main__":
-    pie=batext('hi','es')
-    print(pie)
-    pie=jsontext('hi','es')
-    print(pie)
-    pie=csvtext('hi')
-    print(pie)
+    indict = {}
+    with open('file.csv', mode='r') as csvFile:
+        reader=csv.reader(csvFile)
+        dict_from_csv={rows[0]:rows[1] for rows in reader}
+    ntext=dict_from_csv["Original"]
+    lang=dict_from_csv["Language"]
 
-
-
+    pie=csvtext(ntext.lstrip(" "),lang.lstrip(" "))
+    
